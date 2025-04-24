10965f7f0f009ba8cb46eb0408bddcbdfef1a225
luosong <luosong@cmiot.chinamobile.com>
2025-04-24 11:29:13 +0800

    React项目初始化模板

--- config/layoutConfig.ts
+++ config/layoutConfig.ts
@@ -1,6 +1,28 @@
 import favicon from '@/assets/favicon.ico'
 
-export const layoutConfig = {
+interface LayoutConfigProps {
+  logo: string;
+  menu: {
+    locale: boolean,
+  },
+  layout: 'mix' | 'top' | 'side';
+  token: {
+    header: {
+      heightLayoutHeader: number;
+    }
+  };
+  contentWidth: 'Fluid' | 'Fixed';
+  fixedHeader: boolean;
+  fixSiderbar: boolean;
+  splitMenus: boolean;
+  locale: 'zh-CN' | 'en-US' | 'zh-TW';
+  pwa: boolean;
+  title: string;
+  collapsed: boolean;
+  collapsedButtonRender: () => null,
+}
+
+export const layoutConfig: LayoutConfigProps = {
   logo: favicon,
   menu: {
     locale: false,
@@ -18,7 +40,7 @@ export const layoutConfig = {
   pwa: false,
   // 当显示模式是mix时，开启此配置项会让顶部菜单和侧边菜单保持分离，侧边菜单只展示二级菜单
   splitMenus: false,
-  locale: false, // 国际化
+  locale: "zh-CN", // 国际化
   title: 'OneLink-AIOT',
   collapsed: false,
   collapsedButtonRender: () => null,
